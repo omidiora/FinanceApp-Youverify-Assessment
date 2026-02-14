d2b73f0ed2440fed08442eb1699c4617cce2dfb2
Omidiora Emmanuel <omidioraemmanuel@gmail.com>
2026-02-14 07:06:30 +0100

    Initial Commit

---
+++ FinanceApp/FinanceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceAppApp.swift
+//  FinanceApp
+//
+//  Created by Omidiora Emmanuel on 14/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct FinanceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
